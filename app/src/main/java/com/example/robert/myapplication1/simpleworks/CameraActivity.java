29bd2d35d4f5d73be3a76f19ea67c2d9b93b08a3
Robert <robbooda@gmail.com>
2018-04-26 19:30:47 +0200

    DataBase - File + IMAGE write open v1

--- app/src/main/java/com/example/robert/myapplication1/simpleworks/CameraActivity.java
+++ app/src/main/java/com/example/robert/myapplication1/simpleworks/CameraActivity.java
@@ -2,13 +2,14 @@ package com.example.robert.myapplication1.simpleworks;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
@@ -16,9 +17,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.robert.myapplication1.R;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Robert on 2018-02-13.
@@ -31,10 +43,20 @@ public class CameraActivity extends AppCompatActivity{
     ImageView cameraImage;
     public static final int CAMERA_PERMISSION_REQUEST = 666;
 
+    @BindView(R.id.buttonShowImage)
+    TextView buttonShowImage;
+
+    private File mypath;
+    private File directory;
+    private File f;
+    private Bitmap b;
+    private ImageView img;
+
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        ButterKnife.bind(this);
 
         cameraButton = (Button) findViewById(R.id.buttonToFoto);
         cameraImage = (ImageView) findViewById(R.id.imageViewToCamera);
@@ -125,8 +147,55 @@ public class CameraActivity extends AppCompatActivity{
                 Bundle bundle = data.getExtras();
                 Bitmap bitmap = (Bitmap)  bundle.get("data");
                 cameraImage.setImageBitmap(bitmap);
+
+                saveToInternalStorage(bitmap);
             }
         }
     }
+
+    private void saveToInternalStorage(Bitmap bitmapImage){
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        mypath = new File(directory,"MyImage1.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            Toast.makeText(this,"Photo saved",Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Toast.makeText(this,directory.getAbsolutePath().toString(),Toast.LENGTH_LONG).show();
+        //return directory.getAbsolutePath();
+    }
+
+
+    @OnClick(R.id.buttonShowImage)
+    public void loadImageFromStorage () {
+
+        try {
+            f = new File(directory, "MyImage1.jpg");
+            b = BitmapFactory.decodeStream(new FileInputStream(f));
+            img = (ImageView) findViewById(R.id.imageFromMemory);
+            img.setImageBitmap(b);
+            Toast.makeText(this,directory.getAbsolutePath().toString(),Toast.LENGTH_LONG).show();
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
 
 }
